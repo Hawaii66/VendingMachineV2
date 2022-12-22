e8e3ae02a21c4f418ca4347b23edaf3c0c86e85d
Sebastian Ahlman <hawaiilive@outlook.com>
2022-12-22 01:09:59 +0100

    update url

--- vendingmachine/Utils/API.ts
+++ vendingmachine/Utils/API.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const API_URL = dev
   ? "http://localhost:3000"
-  : "https://hawaiidev.vercel.app";
+  : "https://hawaiisnack.vercel.app";
